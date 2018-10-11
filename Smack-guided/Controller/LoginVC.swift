c4ddf2824a4eefc935845c2d1eb57f6495d0f40a
Kyle Johannsen <kylejohannsen@Kyles-MacBook-Air.local>
2018-10-10 21:58:29 -0400

    Added Login VC and segues

---
+++ Smack-guided/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack-guided
+//
+//  Created by Kyle Johannsen on 10/10/18.
+//  Copyright © 2018 Kyle Johannsen. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    // MARK: Actions
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack-guided/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack-guided
+//
+//  Created by Kyle Johannsen on 10/10/18.
+//  Copyright © 2018 Kyle Johannsen. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Segues
+let TO_LOGIN = "toLogin"
